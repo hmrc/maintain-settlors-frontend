505ba03ad3a905508ab525b8c78442011eb39747
matthew-gibson-18 <175828706+matthew-gibson-18@users.noreply.github.com>
2026-01-22 14:42:15 +0000

    DDCE-7773 Migrate to new Service Navigation Component

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,15 +2,15 @@ import sbt.*
 
 object AppDependencies {
 
-  val bootstrapVersion = "9.19.0"
-  val mongoVersion = "2.10.0"
+  val bootstrapVersion = "10.5.0"
+  val mongoVersion = "2.12.0"
 
   private lazy val compile = Seq(
     play.sbt.PlayImport.ws,
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-30"                     % mongoVersion,
-    "uk.gov.hmrc"             %% "play-frontend-hmrc-play-30"             % "12.18.0",
+    "uk.gov.hmrc"             %% "play-frontend-hmrc-play-30"             % "12.28.0",
     "uk.gov.hmrc"             %% "domain-play-30"                         % "11.0.0",
-    "uk.gov.hmrc"             %% "play-conditional-form-mapping-play-30"  % "3.3.0",
+    "uk.gov.hmrc"             %% "play-conditional-form-mapping-play-30"  % "3.4.0",
     "uk.gov.hmrc"             %% "bootstrap-frontend-play-30"             % bootstrapVersion
   )
 
