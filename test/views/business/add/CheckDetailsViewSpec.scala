fe5375edabd60f69248a4c7fb433016019e38d91
Michael Sweeton <34985534+machinesleet@users.noreply.github.com>
2026-02-06 11:01:41 +0000

    DDCE-7806 tidy

--- test/views/business/add/CheckDetailsViewSpec.scala
+++ test/views/business/add/CheckDetailsViewSpec.scala
@@ -13,8 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package views.business.add
 
+package views.business.add
 
 
 import play.twirl.api.HtmlFormat
